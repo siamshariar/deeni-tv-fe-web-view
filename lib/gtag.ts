c3dc65f00039a3875f060a6d4e22199ff89f3515
siamshariar <siam.it.int@gmail.com>
2026-03-16 11:23:41 +0600

    Use optional chaining for gtag calls

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -15,7 +15,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
 // log a pageview
 export const pageview = (url: string) => {
   if (!GA_TRACKING_ID) return;
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag?.('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
@@ -28,7 +28,7 @@ export const event = ({ action, category, label, value }: {
   value?: number;
 }) => {
   if (!GA_TRACKING_ID) return;
-  window.gtag('event', action, {
+  window.gtag?.('event', action, {
     event_category: category,
     event_label: label,
     value: value,
